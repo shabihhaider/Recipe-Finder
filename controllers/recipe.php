686d6d7a4823024562608fdad9c6d6de407e898f
shabihhaider <2022cs723@student.uet.edu.pk>
2024-09-06 02:36:32 -0700

    Handle Authorization

--- controllers/recipe.php
+++ controllers/recipe.php
@@ -5,7 +5,20 @@ $config = require("config.php");
 $db = new Database($config["database"]);
 
 $heading = "Saved Recipes";
+$currentUserId = 1;
 
-$recipe = $db->query("SELECT * FROM saved_recipes WHERE id = :id", [':id' => $_GET['id']])->fetch();
+$recipe = $db->query("SELECT * FROM saved_recipes WHERE id = :id", [
+    ':id' => $_GET['id']
+])->fetch();
+
+// If the recipe does not exist
+if (! $recipe) {
+    abort();
+}
+
+// If the user is not the current user
+if ($recipe['user_id'] !== $currentUserId) {
+    abort(Response::FORBIDDEN);
+}
 
 require "views/recipe.view.php";
