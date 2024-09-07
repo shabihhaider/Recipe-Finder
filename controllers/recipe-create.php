3baebe52f158b0e054c0d938e46815009425bf58
shabihhaider <2022cs723@student.uet.edu.pk>
2024-09-06 22:55:48 -0700

    Add Validation

--- controllers/recipe-create.php
+++ controllers/recipe-create.php
@@ -6,10 +6,25 @@ $db = new Database($config["database"]);
 $heading = "Create a Recipe";
 
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $db->query("INSERT INTO saved_recipes (recipe_name, user_id) VALUES (:recipe_name, :user_id)", [
-        ':recipe_name' => $_POST['recipe'],
-        ':user_id' => 1
-    ]);
+
+    $errors = [];
+
+    if(strlen($_POST['recipe']) === 0) {
+        $errors['recipe'] = "Recipe name is required";
+    }
+
+    if(strlen($_POST['recipe']) > 100) {
+        $errors['recipe'] = "Recipe name is less than 100 characters";
+    }
+
+    // If there are no errors, then save the recipe
+    if(empty($errors)) {
+        $db->query("INSERT INTO saved_recipes (recipe_name, user_id) VALUES (:recipe_name, :user_id)", [
+            ':recipe_name' => $_POST['recipe'],
+            ':user_id' => 1
+        ]);
+    }
+    
 }
 
 require "views/recipe-create.view.php";
--- views/recipe-create.view.php
+++ views/recipe-create.view.php
@@ -13,7 +13,12 @@
               <div class="col-span-full">
                 <label for="recipe" class="block text-sm font-medium leading-6 text-gray-900">Recipe Name</label>
                 <div class="mt-2">
-                  <textarea id="recipe" name="recipe" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                  <textarea id="recipe" name="recipe" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"><?= isset($_POST['recipe']) ? $_POST['recipe'] : '' ?></textarea>
+                  
+                  <?php if (isset($errors['recipe'])) : ?>
+                    <p class="text-red-500 text-xs mt-2"><?= $errors['recipe'] ?></p>
+                  <?php endif; ?>
+
                 </div>
                 <p class="mt-3 text-sm leading-6 text-gray-600">Add a Recipe Name.</p>
               </div>
